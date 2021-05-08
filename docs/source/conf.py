5c00611e85ba2188448474d4bacac0099fa47ba8
vicjan89 <vicjan89@gmail.com>
2021-05-09 00:58:06 +0300

    Изменил настройки PDF повторно

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, РУП "Витебскэнерго"'
 author = 'РУП "Витебскэнерго"'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1'
+release = '1.15'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +30,8 @@ release = '1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['rst2pdf.pdfbuilder']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
